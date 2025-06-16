96e58b2fdbfcf1af5bd5a1ab3ef4b27d3dd88cda
JaehyunLee <ljh@JaehyunLees-MacBook-Pro.local>
2025-06-16 13:16:57 +0900

    feat: home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,17 @@
-import Image from "next/image";
+import PocketmonCard from "./components/PocketmonCard";
 
-export default function Home() {
-  return <div></div>;
+export default async function Home() {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+  const res = await fetch(`${baseUrl}/api/pocketmon`);
+  const { data: pocketmons } = await res.json();
+
+  return (
+    <div className="flex flex-wrap justify-center gap-5">
+      {pocketmons
+        ? pocketmons.map((pocketmon) => (
+            <PocketmonCard key={pocketmon.id} pocketmon={pocketmon} />
+          ))
+        : ""}
+    </div>
+  );
 }
