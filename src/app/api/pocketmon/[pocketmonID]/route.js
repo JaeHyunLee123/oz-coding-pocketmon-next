7b922945417c0209a95d795365f8e4c957302231
JaehyunLee <ljh@JaehyunLees-MacBook-Pro.local>
2025-06-16 14:14:09 +0900

    feat: pocketmon detail api

---
+++ src/app/api/pocketmon/[pocketmonID]/route.js
@@ -0,0 +1,13 @@
+/** @typedef {import("../type").PocketmonData} PocketmonData */
+
+import { fetchPocketmonById } from "@/app/libs/utils";
+
+export async function GET(request) {
+  const splitedURL = request.url.split("/");
+
+  const pocketmonID = splitedURL[splitedURL.length - 1];
+
+  const data = await fetchPocketmonById(pocketmonID);
+
+  return Response.json({ data });
+}
