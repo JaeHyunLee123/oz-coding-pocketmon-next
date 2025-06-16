ba7fea97e4dc0a35816ba1aaa8540fe4378b35d1
JaehyunLee <ljh@JaehyunLees-MacBook-Pro.local>
2025-06-16 11:36:26 +0900

    feat: pocketmon data type

---
+++ src/types.js
@@ -0,0 +1,10 @@
+/**
+ * @typedef {Object} PocketmonData
+ * @property {number} id
+ * @property {string} name
+ * @property {string} description
+ * @property {string} frontImage
+ * @property {string} backImage
+ */
+
+export {};
